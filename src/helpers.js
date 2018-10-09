21e5421ae097755a4504e620c1e632e390db94e1
「厚キツネ」 <dev@thicc.cat>
2018-10-09 03:27:27 +0300

    fix travis and lint

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 const apiBase = 'https://api.codebottle.io/';
 
 module.exports = {
-	apiUrl: url => apiBase + (url.indexOf('/') === 0 ? url.substr(1) : url),
+  apiUrl: url => apiBase + (url.indexOf('/') === 0 ? url.substr(1) : url),
 };
