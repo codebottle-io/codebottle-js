55cd6ffe2774f91be9b283bb352ac6a00e26846b
「厚キツネ」 <dev@thicc.cat>
2018-10-10 20:55:13 +0300

    fix api

--- src/api.js
+++ src/api.js
@@ -6,20 +6,20 @@ const reflectors = ['toString', 'valueOf', custom, Symbol.toPrimitive];
 
 const traps = {
   get(path, name) {
-    if (reflectors.includes(name)) return () => path.join('/');
+    if (reflectors.includes(name)) return () => path().join('/');
 
     if (name in fetch) {
       return (query = {}) =>
-        new fetch(name.toUpperCase(), path.join('/'), { query, headers: HEADERS })
+        new fetch(name.toUpperCase(), path().join('/'), { query, headers: HEADERS })
           .then(res => res.body);
     }
 
-    return new Proxy([...path, name], traps);
+    return new Proxy(() => [...path(), name], traps);
   },
 
   apply(path, _, args) {
-    return new Proxy([...path, ...args], traps);
+    return new Proxy(() => [...path(), ...args], traps);
   },
 };
 
-module.exports = new Proxy([DOMAIN], traps);
+module.exports = new Proxy(() => [DOMAIN], traps);
